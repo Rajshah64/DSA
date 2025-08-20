f69da111cbd3960f4e5326e53585550dca28d36e
Raj shah <rajviralshah@gmail.com>
2025-08-20 22:11:51 +0530

    20/08/2025 --> 5

--- Graphs/BFSandDFS/BipartiteGraph/BipartiteGraph.cpp
+++ Graphs/BFSandDFS/BipartiteGraph/BipartiteGraph.cpp
@@ -1,51 +1,76 @@
-#include <bits/stdc++.h>
+// Problem number 785 on leetcode .
 
-using namespace std;
+// this is the dfs approach
+//  class Solution
+//  {
+//      private:
+//          bool dfs(vector<vector<int>>& graph, vector<int>& color, int node , int col){
+//              color[node]=col;
+//              for(auto it: graph[node]){
+//                  if(color[it]==-1){
+//                      if(!dfs(graph,color,it, !col)){
+//                          return false;
+//                      }
 
+//                 }else if(color[it]==col){
+//                         return false;
+//                     }
+//             }
+//             return true;
+//         }
+//     public:
+//     bool isBipartite(vector<vector<int>> &graph){
+//         int n=graph.size();
+//         vector<int> color(n,-1);
+//         bool ans;
+//         for(int i=0;i<n;i++){
+//             if(color[i]==-1){
+//                 if (!dfs(graph, color, i, 0)) {
+//                     return false;
+//                 }
+//             }
+//         }
+//         return true;
+//     }
+// };
+
+// This is the bfs approach
 class Solution
 {
-private:
-    bool dfs(vector<vector<int>> &graph, vector<int> &color, int node, int col)
-    {
-        color[node] = col;
-        for (auto it : graph[node])
-        {
-            if (color[it] == -1)
-            {
-                if (!dfs(graph, color, it, !col))
-                {
-                    return false;
-                }
-            }
-            else if (color[it] == col)
-            {
-                return false;
-            }
-        }
-        return true;
-    }
-
 public:
     bool isBipartite(vector<vector<int>> &graph)
     {
         int n = graph.size();
         vector<int> color(n, -1);
-        bool ans;
-        for (int i = 0; i < n; i++)
+
+        for (int start = 0; start < n; start++)
         {
-            if (color[i] == -1)
+            if (color[start] == -1)
             {
-                if (!dfs(graph, color, i, 0))
+                queue<int> q;
+                q.push(start);
+                color[start] = 0;
+
+                while (!q.empty())
                 {
-                    return false;
+                    int node = q.front();
+                    q.pop();
+
+                    for (auto it : graph[node])
+                    {
+                        if (color[it] == -1)
+                        {
+                            color[it] = !color[node];
+                            q.push(it);
+                        }
+                        else if (color[it] == color[node])
+                        {
+                            return false;
+                        }
+                    }
                 }
             }
         }
         return true;
     }
 };
-
-int main()
-{
-    return 0;
-}
