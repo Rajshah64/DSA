40caeade99ab6abb6eb2f3de127a66b902a8a302
Raj shah <rajviralshah@gmail.com>
2025-09-04 18:31:57 +0530

    04/09/2025 --> 1

---
+++ Recursion/Basics/SubsequencesWhoseSumisK/SubsequencesWhoseSumisK.cpp
@@ -0,0 +1,148 @@
+// Remember this method this is very important .
+
+// This method is a bit time consuming .
+// The better approach would be to take sum as a parameter and check if it is equal to k or not.
+// and every time you call recursive function , at that time you can pass the updated sum.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void findSubsequences(int curr, vector<int> &arr,
+                      vector<int> &subarr, vector<vector<int>> &res)
+{
+
+    // Base case: When we reach the end of the array,
+    // add the current subsequence to the result
+
+    if (curr == arr.size())
+    {
+        int sum = 0;
+        for (int i = 0; i < subarr.size(); i++)
+        {
+            sum += subarr[i];
+        }
+        if (sum == 2)
+            res.push_back(subarr);
+        return;
+    }
+
+    //  Include the current element in the subsequence
+    subarr.push_back(arr[curr]);
+
+    // Recurse to the next element
+    findSubsequences(curr + 1, arr, subarr, res);
+
+    // Backtrack: Remove the current element and explore
+    // the next possibility
+    subarr.pop_back();
+
+    //  Do not include the current element
+    // in the subsequence
+    findSubsequences(curr + 1, arr, subarr, res);
+}
+
+int main()
+{
+    vector<int> arr = {1, 2, 1};
+    int n = arr.size();
+
+    vector<int> subarr;
+    vector<vector<int>> res;
+
+    findSubsequences(0, arr, subarr, res);
+
+    for (int i = 0; i < res.size(); i++)
+    {
+        for (int j = 0; j < res[i].size(); j++)
+        {
+            cout << res[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+
+// This method is if you want to print only one ans.
+// How it works is if we find a valid subsequence, we immediately return true.
+// Thus the other half of the recursive tree is never executed, thus is faster .
+// Remember this method this is very important .
+
+#include <bits/stdc++.h>
+using namespace std;
+
+bool findSubsequences(int curr, vector<int> &arr,
+                      vector<int> &subarr, vector<vector<int>> &res)
+{
+
+    // Base case: When we reach the end of the array,
+    // add the current subsequence to the result
+
+    if (curr == arr.size())
+    {
+        int sum = 0;
+        for (int i = 0; i < subarr.size(); i++)
+        {
+            sum += subarr[i];
+        }
+        if (sum == 2)
+        {
+            res.push_back(subarr);
+            return true;
+        }
+        else
+            return false;
+    }
+
+    //  Include the current element in the subsequence
+    subarr.push_back(arr[curr]);
+
+    // Recurse to the next element
+    if (findSubsequences(curr + 1, arr, subarr, res))
+    {
+        return true;
+    }
+
+    // Backtrack: Remove the current element and explore
+    // the next possibility
+    subarr.pop_back();
+
+    //  Do not include the current element
+    // in the subsequence
+    if (findSubsequences(curr + 1, arr, subarr, res) == true)
+    {
+        return true;
+    }
+
+    return false;
+}
+
+int main()
+{
+    vector<int> arr = {1, 2, 1};
+    int n = arr.size();
+
+    vector<int> subarr;
+    vector<vector<int>> res;
+
+    findSubsequences(0, arr, subarr, res);
+
+    for (int i = 0; i < res.size(); i++)
+    {
+        for (int j = 0; j < res[i].size(); j++)
+        {
+            cout << res[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+
+
+
+//The next main concept is count subsequences.
+/*
+    the main structure will be 
+    
+*/
