7981ab623492970090ee27a764dd0c99df2329a2
Raj shah <rajviralshah@gmail.com>
2025-09-12 13:08:48 +0530

    12/09/2025 --> 3

---
+++ Recursion/Permutation/Permutation.cpp
@@ -0,0 +1,28 @@
+// Leetcode question 46
+
+class Solution
+{
+private:
+    void permutation(int ind, vector<int> &nums, vector<vector<int>> &ans)
+    {
+        if (ind == nums.size())
+        {
+            ans.push_back(nums);
+            return;
+        }
+        for (int i = ind; i < nums.size(); i++)
+        {
+            swap(nums[ind], nums[i]);
+            permutation(ind + 1, nums, ans);
+            swap(nums[ind], nums[i]);
+        }
+    }
+
+public:
+    vector<vector<int>> permute(vector<int> &nums)
+    {
+        vector<vector<int>> ans;
+        permutation(0, nums, ans);
+        return ans;
+    }
+};
