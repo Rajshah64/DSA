1152db38a15287893cef8fc3411c07d6bdfd7174
Raj shah <rajviralshah@gmail.com>
2025-09-12 19:07:35 +0530

    12/09/2025 --> 4

--- Recursion/Permutation/Permutation.cpp
+++ Recursion/Permutation/Permutation.cpp
@@ -1,20 +1,50 @@
 // Leetcode question 46
+// time complexity: O(n*n!)
+// space complexity: O(n) + O(n!) for recursion stack space and answer array
+// class Solution {
+//     private:
+//         void permutation(int ind, vector<int>& nums, vector<vector<int>>& ans){
+//             if (ind == nums.size()) {
+//             ans.push_back(nums);
+//             return;
+//             }
+//             for (int i = ind; i < nums.size(); i++) {
+//                 swap(nums[ind], nums[i]);
+//                 permutation(ind + 1, nums, ans);
+//                 swap(nums[ind], nums[i]);
+//             }
+//         }
+// public:
+//     vector<vector<int>> permute(vector<int>& nums) {
+//         vector<vector<int>> ans;
+//         permutation(0,nums,ans);
+//         return ans;
+//     }
+// };
 
+// time complexity: O(n*n!)
+// space complexity: O(n) + O(n) for recursion stack space and frequency array
 class Solution
 {
 private:
-    void permutation(int ind, vector<int> &nums, vector<vector<int>> &ans)
+    void permutation(vector<int> &nums, vector<vector<int>> &ans, vector<int> &temp, vector<int> &freq)
     {
-        if (ind == nums.size())
+        if (temp.size() == nums.size())
         {
-            ans.push_back(nums);
+            ans.push_back(temp);
             return;
         }
-        for (int i = ind; i < nums.size(); i++)
+
+        for (int i = 0; i < nums.size(); i++)
         {
-            swap(nums[ind], nums[i]);
-            permutation(ind + 1, nums, ans);
-            swap(nums[ind], nums[i]);
+            if (!freq[i])
+            {
+                temp.push_back(nums[i]);
+                freq[i] = 1;
+                permutation(nums, ans, temp, freq);
+                freq[i] = 0;
+                temp.pop_back();
+            }
         }
     }
 
@@ -22,7 +52,9 @@ public:
     vector<vector<int>> permute(vector<int> &nums)
     {
         vector<vector<int>> ans;
-        permutation(0, nums, ans);
+        vector<int> temp;
+        vector<int> freq(nums.size(), 0);
+        permutation(nums, ans, temp, freq);
         return ans;
     }
 };
