4dac02e6773784939e9f5d9601a3f3b7d505de6f
Raj shah <rajviralshah@gmail.com>
2026-02-02 18:25:04 +0530

    02.02.2026 --> 4

--- Arrays/Medium/1.2SumProblem/2SumProblem.cpp
+++ Arrays/Medium/1.2SumProblem/2SumProblem.cpp
@@ -25,3 +25,27 @@ public:
 // Time complexity:- O(N^2)
 // Space complexity:- O(1)
 
+// Optimised Approach
+class Solution
+{
+public:
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+        int n = nums.size();
+        unordered_map<long long, int> mp;
+        for (int i = 0; i < n; i++)
+        {
+            int rem = target - nums[i];
+            // This is the remainder we have to search in the map.
+            if (mp.find(rem) != mp.end())
+            {
+                // You found the pair
+                return {mp[rem], i};
+            }
+            mp[nums[i]] = i;
+        }
+        return {-1, -1};
+    }
+};
+// Time Complexity:O(n)
+// Space Complexity:O(n)
