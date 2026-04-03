32f4fb797005cbac5bb5be40d4f9d84cb42154a2
Raj shah <rajviralshah@gmail.com>
2026-04-03 19:22:49 +0530

    03/04/2026 --> 1

---
+++ LinkedList/REVISION/MediumProblemsofLL/3.ReverseALL/ReverseaLL.cpp
@@ -0,0 +1,29 @@
+// Leetcode Question 206.
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution
+{
+public:
+    ListNode *reverseList(ListNode *head)
+    {
+        ListNode *p = nullptr;
+        ListNode *q = head;
+        while (q)
+        {
+            ListNode *temp = q->next;
+            q->next = p;
+            p = q;
+            q = temp;
+        }
+        return p;
+    }
+};
